376136ef40456ca3527fc29e7141cdd191e75de7
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-10-14 18:07:08 -0400

    Update gcp-commands.sh

--- gcp-commands.sh
+++ gcp-commands.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-PROJECT="blazor-sample"
+# PROJECT_ID-variable will be set to a concatenation of "blazor-sample-" and your username to ensure the ID is unique accross accounts
+PROJECT_ID="blazor-sample-$USER"
 SERVICE_ACCOUNT="blazor-sample-service-account"
 AUTH_FILE="blazor-sample-auth.json"
 REGION="us-east1"
 
 # create a project to hold cloud resources
-gcloud projects create $PROJECT
+gcloud projects create $PROJECT_ID
 # switch to this project, this will make sure subsequent commands will use this project
-gcloud config set project $PROJECT
+gcloud config set project $PROJECT_ID
 
 # Firestore requires an App Engine in your project
 gcloud app create --region=$REGION
@@ -19,9 +20,9 @@ gcloud firestore databases create --region=$REGION
 # create a service account to authenticate against the Firestore
 gcloud iam service-accounts create $SERVICE_ACCOUNT
 # give service account owner-role of the project
-gcloud projects add-iam-policy-binding $PROJECT --member "serviceAccount:$SERVICE_ACCOUNT@$PROJECT.iam.gserviceaccount.com" --role "roles/owner"
+gcloud projects add-iam-policy-binding $PROJECT_ID --member "serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" --role "roles/owner"
 
 # create a json-authentication file to authenticate as service account
-gcloud iam service-accounts keys create $AUTH_FILE --iam-account "$SERVICE_ACCOUNT@$PROJECT.iam.gserviceaccount.com"
+gcloud iam service-accounts keys create $AUTH_FILE --iam-account "$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com"
 # download the json-authentication file to your machine
 cloudshell download $AUTH_FILE
